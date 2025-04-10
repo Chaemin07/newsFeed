97e0c1a717132155df1442f74715d102c17026b5
sconers00 <jungnam369@naver.com>
2025-04-10 10:35:33 +0900

    merge

---
+++ src/main/java/com/example/newsfeedproject/auth/Dto/LoginRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.newsfeedproject.auth.Dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 로그인 요청 시 클라이언트로부터 전달받는 사용자 입력 정보를 담는 DTO 클래스입니다.
+ * <p>
+ * 사용자 ID와 비밀번호를 포함하며, 이 정보를 통해 로그인 인증을 수행합니다.
+ * </p>
+ */
+@Getter
+@RequiredArgsConstructor
+public class LoginRequestDto {
+    /**
+     * 사용자의 로그인 ID
+     */
+    public final String userId;
+    /**
+     * 사용자의 비밀번호
+     */
+    public final String userPw;
+
+}
