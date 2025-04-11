b1a4a36b1fcb4c1ff12decec1dcf411cdd3162b2
sconers00 <jungnam369@naver.com>
2025-04-11 18:10:38 +0900

    issue : merge conflict, do not PR this

--- src/main/java/com/example/newsfeedproject/comment/entity/Comment.java
+++ src/main/java/com/example/newsfeedproject/comment/entity/Comment.java
@@ -1,6 +1,6 @@
 package com.example.newsfeedproject.comment.entity;
 
-
+import com.example.newsfeedproject.common.entity.BaseEntity;
 import com.example.newsfeedproject.feed.entity.NewsFeed;
 import com.example.newsfeedproject.user.entity.User;
 import jakarta.persistence.*;
